c7a43e1d0d566154f8be2acce11f27affbc3b190
Douglas Arsego <douglasarsego14@gmail.com>
2023-04-11 09:14:42 -0300

    Repositorio

--- Aula04-AJAX/numeros.php
+++ Aula04-AJAX/numeros.php
@@ -1,10 +1,9 @@
 <?php
 
-    $numero = $_GET['numero'];
+    $numero = $_GET["numero"];
     $texto = "";
     for( $i = 1; $i <= $numero; $i++){
         $texto = $texto."<br>".$i;
-
     }
     echo $texto;
 
